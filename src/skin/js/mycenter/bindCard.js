48473f3a394538b023688e513e5be8e928780d78
suzhihui <wisdom.su@163.com>
2017-02-18 18:33:25 +0800

    bindCard

--- src/skin/js/mycenter/bindCard.js
+++ src/skin/js/mycenter/bindCard.js
@@ -5,24 +5,24 @@
 var bindCardModel = new Vue({
   el: '#model-bindCard',
   data: {
-    orderId:'',
-    orderInfo:'',
+    orderId: '',
+    orderInfo: '',
     bankSelected: '0', // 银行
-    authen:'', // 用户信息
-    provinceS:'0', // 选中的省份
-    cityS:'', // 选中的城市
-    province:'', 
-    city:'',
-    backCode:'', // 银行卡号
-    masterBank:'', // 支行名称
-    tel:'', // 预留手机号
+    authen: '', // 用户信息
+    provinceS: '0', // 选中的省份
+    cityS: '', // 选中的城市
+    province: '',
+    city: '',
+    backCode: '', // 银行卡号
+    masterBank: '', // 支行名称
+    tel: '', // 预留手机号
     showProvince: false,
-    showCity:false,
-    buyLock:false,
-    
-    
-    
-    
+    showCity: false,
+    buyLock: false,
+
+
+
+
 
 
     // 银行信息
@@ -96,15 +96,16 @@ var bindCardModel = new Vue({
     }]
   },
   methods: {
-    getOrderInfo: function  () {
+    getOrderInfo: function () {
       var _this = this;
       // 检测登录
-      if (!headerModel.isLogin) {;
+      if (!headerModel.isLogin) {
+        ;
         DGDTOOLS.check._isLogin();
         return;
-      } else{
+      } else {
         $('#loader').fadeIn(300);
-        this.$http.get(headerModel.api + '/Order/GetOrder?orderid='+ this.orderId).then(function (response) {
+        this.$http.get(headerModel.api + '/Order/GetOrder?orderid=' + this.orderId).then(function (response) {
           $('#loader').fadeOut(300);
           _this.orderInfo = response.data;
         })
@@ -119,78 +120,85 @@ var bindCardModel = new Vue({
       })
     },
     // 城市二级联动
-    getCity: function  (id) {
+    getCity: function (id) {
       var _this = this;
-      if(id!=0){
-        headerModel.loading=true;
+      if (id != 0) {
+        headerModel.loading = true;
       }
       _this.$http.get(headerModel.api + '/Help/GetCity?provinceid=' + id).then(function (response) {
-        headerModel.loading=false;
+        headerModel.loading = false;
         _this.showProvince = true;
-        if(id!=0){
+        if (id != 0) {
           _this.city = response.data.cityList;
           _this.cityS = '0';
-        }else{
+        } else {
           _this.province = response.data.cityList;
-          
+
         }
       })
     },
-    getCityStr: function  () {
+    getCityStr: function () {
       var _this = this,
-          str = '';
-      if(_this.provinceS && _this.cityS){
-        for(var i=0; i<_this.province.length; i++){
-          if(_this.province[i].CityID === _this.provinceS){
+        str = '';
+      if (_this.provinceS && _this.cityS) {
+        for (var i = 0; i < _this.province.length; i++) {
+          if (_this.province[i].CityID === _this.provinceS) {
             str += _this.province[i].CityName + ',';
             break;
           }
         }
-        for(var i=0; i<_this.city.length; i++){
-          if(_this.city[i].CityID === _this.cityS){
+        for (var i = 0; i < _this.city.length; i++) {
+          if (_this.city[i].CityID === _this.cityS) {
             str += _this.city[i].CityName;
             break;
           }
-        }        
+        }
       }
       return str;
     },
-    bindFn: function  () {
+    bindCh: function () {
       var _this = this;
-      if(_this.bankSelected == '0'){
+      if (_this.bankSelected == '0') {
         DGDTOOLS.tip._tip('请选择开户银行卡')
-        return;
+        return false;
       }
-      if(_this.backCode ==''){
-        DGDTOOLS.tip._tip('请输入银行卡号',function  () {
+      if (_this.backCode == '') {
+        DGDTOOLS.tip._tip('请输入银行卡号', function () {
           _this.$refs.backCode.focus();
         })
-        return
+        return false;
       }
-      if(isNaN(_this.backCode)|| _this.backCode.length<16){
-        DGDTOOLS.tip._tip('银行卡号输入不合法',function  () {
+      if (isNaN(_this.backCode) || _this.backCode.length < 16) {
+        DGDTOOLS.tip._tip('银行卡号输入不合法', function () {
           _this.$refs.backCode.focus();
         })
-        return
+        return false;
       }
-      if(!_this.provinceS || !_this.cityS){
+      if (!_this.provinceS || !_this.cityS) {
         DGDTOOLS.tip._tip('请选择银行卡开户省市')
-        return;
+        return false;
       }
-      if(_this.masterBank == ''){
-        DGDTOOLS.tip._tip('请输入支行名称',function  () {
+      if (_this.masterBank == '') {
+        DGDTOOLS.tip._tip('请输入支行名称', function () {
           _this.$refs.masterBank.focus();
         })
+        return false;
+      }
+      return true;
+    },
+    bindFn: function () {
+      var _this = this;
+      if(!_this.bindCh()){
         return;
       }
-      if(_this.tel == ''){
-        DGDTOOLS.tip._tip('请输入银行卡预留手机号',function  () {
+      if (_this.tel == '') {
+        DGDTOOLS.tip._tip('请输入银行卡预留手机号', function () {
           _this.$refs.tel.focus();
         })
         return;
       }
-      if(!DGDTOOLS.check._isPhone(_this.tel)){
-        DGDTOOLS.tip._tip('预留手机号不合法！',function  () {
+      if (!DGDTOOLS.check._isPhone(_this.tel)) {
+        DGDTOOLS.tip._tip('预留手机号不合法！', function () {
           _this.$refs.tel.focus();
         })
         return;
@@ -206,44 +214,44 @@ var bindCardModel = new Vue({
             // 前端验证通过 可以提交了。。。
             if (_this.buyLock) {
               return;
-            }else{
+            } else {
               _this.buyLock = true;
             }
 
             // 提交
 
             $.ajax({
-                url: headerModel.api + '/Pay/Deal',
-                data: {
-                  "PayCode": _this.bankListMap[_this.bankSelected].payCode, // 银行的编号
-                  "AccNo": _this.backCode, // 银行卡号
-                  "IdHolder": _this.authen.FullName, // 持卡人（真实姓名）
-                  "IdCard":  _this.authen.IDNumber,// 身份证
-                  "Mobile": _this.tel,// 手机号码
-                  "TxnAmt": _this.orderInfo.order.PayAmount.toFixed(2),// 交易金额
-                  "TradeNo": _this.orderId,// 订单号
-                  "Token": headerModel.token,
-                  "City": _this.provinceS+','+ _this.cityS, // 省份和城市信息的组合
-                  "BankName": _this.bankListMap[_this.bankSelected].bankName,// 银行名称
-                  "BranchName": _this.masterBank  // 支行名称         
-                },
-                type: 'post',
-                success: function (response) {
-                  if (response.retCode == '0000') {
-                    // 提交成功跳转到订单确认页面，根据定单id
-                    DGDTOOLS.tip._tip('银行卡绑定成功',function () {
-                      // window.location.href="orderConf.html?orderId="+ response.orderid;
-                    })
+              url: headerModel.api + '/Pay/Deal',
+              data: {
+                "PayCode": _this.bankListMap[_this.bankSelected].payCode, // 银行的编号
+                "AccNo": _this.backCode, // 银行卡号
+                "IdHolder": _this.authen.FullName, // 持卡人（真实姓名）
+                "IdCard": _this.authen.IDNumber,// 身份证
+                "Mobile": _this.tel,// 手机号码
+                "TxnAmt": _this.orderInfo.order.PayAmount.toFixed(2),// 交易金额
+                "TradeNo": _this.orderId,// 订单号
+                "Token": headerModel.token,
+                "City": _this.provinceS + ',' + _this.cityS, // 省份和城市信息的组合
+                "BankName": _this.bankListMap[_this.bankSelected].bankName,// 银行名称
+                "BranchName": _this.masterBank  // 支行名称         
+              },
+              type: 'post',
+              success: function (response) {
+                if (response.retCode == '0000') {
+                  // 提交成功跳转到订单确认页面，根据定单id
+                  DGDTOOLS.tip._tip('银行卡绑定成功', function () {
+                    // window.location.href="orderConf.html?orderId="+ response.orderid;
+                  })
 
-                  }else{
-                    DGDTOOLS.tip._tip(response.retMsg)
-                  }
-                },
-                error: function (e) {
-                  DGDTOOLS.tip._tip(e.status + ":" + '接口异常')
-                  console.error(e.status + ":" + e.responseText);
+                } else {
+                  DGDTOOLS.tip._tip(response.retMsg)
                 }
-              })
+              },
+              error: function (e) {
+                DGDTOOLS.tip._tip(e.status + ":" + '接口异常')
+                console.error(e.status + ":" + e.responseText);
+              }
+            })
 
 
           }
@@ -254,16 +262,16 @@ var bindCardModel = new Vue({
         maskClose: true,
         effect: true
       })
-      
-      
-      
-      
-      
+
+
+
+
+
     }
-    
+
   },
   computed: {
-    sheng: function  () {
+    sheng: function () {
       this.$nextTick(function () {
         return this.province; // => '更新完成'
       })
@@ -272,7 +280,7 @@ var bindCardModel = new Vue({
      *  true : 个人中心绑卡
      *  false : 定单绑卡
      */
-    isMC: function(){
+    isMC: function () {
       return this.orderId == 0;
     }
   },
