2d06bec6c870a79e5780d3d40857ecd0c04ed234
suzhihui <wisdom.su@163.com>
2017-02-24 18:20:55 +0800

    sdfsfsf

--- gulpfile-dist.js
+++ gulpfile-dist.js
@@ -1,111 +1,148 @@
- /*
-  * @Author: suzhihui
-  * @Date:   2016-11-17 22:01:02
-  * @Last Modified by:   老苏
-  * @Last Modified time: 2016-12-06 12:44:57
-  */
+/*
+ * @Author: suzhihui
+ * @Date:   2016-11-17 22:01:02
+ * @Last Modified by:   老苏
+ * @Last Modified time: 2016-12-06 12:44:57
+ */
 
- // 'use strict';
+// 'use strict';
 
- var gulp = require('gulp'),
-   sass = require('gulp-sass'),
-   sourcemaps = require('gulp-sourcemaps'),
-   autoprefixer = require('gulp-autoprefixer'),
-   minifycss = require('gulp-minify-css'),
-   uglify = require('gulp-uglify'),
-   rename = require('gulp-rename'),
-   clean = require('gulp-clean'),
-   changed = require('gulp-changed'),
-   debug = require('gulp-debug'),
-   browserSync = require('browser-sync').create(),
-   jade = require('gulp-jade'),
-   base64 = require('gulp-base64');
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  clean = require('gulp-clean'),
+  changed = require('gulp-changed'),
+  debug = require('gulp-debug'),
+  browserSync = require('browser-sync').create(),
+  jade = require('gulp-jade'),
+  base64 = require('gulp-base64');
 
- //样式
- gulp.task('css', function() {
-   return gulp.src(['src/skin/scss/**/*.scss', 'src/skin/comStyle/*.scss'])
-     .pipe(changed('dist/skin/css/', { extension: '.css' }))
-     .pipe(debug({ title: '编译：' }))
-     //  .pipe(sourcemaps.init())
-     // 嵌套输出方式 nested (默认)
-     // 展开输出方式 expanded
-     // 紧凑输出方式 compact
-     // 压缩输出方式 compressed
-     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-     .pipe(autoprefixer({
-       browsers: ['last 2 versions', 'Android >= 4.0', 'IE >= 9'],
-       cascade: true,
-       remove: true
-     }))
-     .pipe(base64({
-       // 排除字体文件
-       exclude: ['.eot', '.woff', '.ttf', '.svg']
-     }))
-     //  .pipe(sourcemaps.write('./maps'))
-     .pipe(gulp.dest('dist/skin/css'))
-     .pipe(browserSync.reload({ stream: true }));
- })
+//样式
+gulp.task('css', ['css-pro'], function () {
+  return gulp.src(['src/skin/scss/**/*.scss', '!src/skin/promotion/**/*.scss'])
+    .pipe(changed('dist/skin/css/', { extension: '.css' }))
+    .pipe(debug({ title: '编译：' }))
+    //  .pipe(sourcemaps.init())
+    // 嵌套输出方式 nested (默认)
+    // 展开输出方式 expanded
+    // 紧凑输出方式 compact
+    // 压缩输出方式 compressed
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'Android >= 4.0', 'IE >= 9'],
+      cascade: true,
+      remove: true
+    }))
+    .pipe(base64({
+      // 排除字体文件
+      exclude: ['.eot', '.woff', '.ttf', '.svg']
+    }))
+    //  .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('dist/skin/css'))
+    .pipe(browserSync.reload({ stream: true }));
+})
 
- //脚本
- gulp.task('js', function() {
-   return gulp.src(['src/skin/js/*.js', 'src/skin/js/**/*.js'])
-     .pipe(changed('dist/skin/js', { extension: '.js' }))
-     .pipe(debug({ title: '编译：' }))
-     // .pipe(rename({ suffix: '.min'}))
-     .pipe(uglify())
-     .pipe(gulp.dest('dist/skin/js'))
- })
+gulp.task('css-pro', function () {
+  return gulp.src(['src/skin/promotion/**/*.scss'])
+    .pipe(changed('dist/skin/promotion/', { extension: '.css' }))
+    .pipe(debug({ title: '编译：' }))
+    //  .pipe(sourcemaps.init())
+    // 嵌套输出方式 nested (默认)
+    // 展开输出方式 expanded
+    // 紧凑输出方式 compact
+    // 压缩输出方式 compressed
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'Android >= 4.0', 'IE >= 9'],
+      cascade: true,
+      remove: true
+    }))
+    .pipe(base64({
+      // 排除字体文件
+      exclude: ['.eot', '.woff', '.ttf', '.svg']
+    }))
+    //  .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('dist/skin/promotion'))
+    .pipe(browserSync.reload({ stream: true }));
+})
+//脚本
+gulp.task('js',['js-pro'], function () {
+  return gulp.src(['src/skin/js/*.js', 'src/skin/js/**/*.js'])
+    .pipe(changed('dist/skin/js', { extension: '.js' }))
+    .pipe(debug({ title: '编译：' }))
+    // .pipe(rename({ suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/skin/js'))
+})
+gulp.task('js-pro', function () {
+  return gulp.src(['src/skin/promotion/**/*.js'])
+    .pipe(changed('dist/skin/promotion', { extension: '.js' }))
+    .pipe(debug({ title: '编译：' }))
+    // .pipe(rename({ suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/skin/promotion/'))
+})
+// 图片
+gulp.task('img',['img-pro'], function () {
+  return gulp.src(['src/skin/img/*', 'src/skin/img/**/*'])
+    // .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/skin/img'))
+  // .pipe(notify({ message: 'Images task complete' }));
+});
 
- // 图片
- gulp.task('img', function() {
-   return gulp.src(['src/skin/img/*', 'src/skin/img/**/*'])
-     // .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-     .pipe(gulp.dest('dist/skin/img'))
-     // .pipe(notify({ message: 'Images task complete' }));
- });
+gulp.task('img-pro', function () {
+  return gulp.src(['src/skin/promotion/**/*'])
+    // .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/skin/promotion'))
+  // .pipe(notify({ message: 'Images task complete' }));
+});
 
- //第三方js
- gulp.task('plugs', function() {
-   return gulp.src(['src/skin/plugs/*', 'src/skin/plugs/**/*'])
-     .pipe(changed('dist/skin/plugs', { extension: '.plugs' }))
-     .pipe(debug({ title: '编译：' }))
-     .pipe(gulp.dest('dist/skin/plugs'))
- })
+//第三方js
+gulp.task('plugs', function () {
+  return gulp.src(['src/skin/plugs/*', 'src/skin/plugs/**/*'])
+    .pipe(changed('dist/skin/plugs', { extension: '.plugs' }))
+    .pipe(debug({ title: '编译：' }))
+    .pipe(gulp.dest('dist/skin/plugs'))
+})
 
- //jade
- gulp.task('jade', function() {
-   return gulp.src(['src/*.jade', 'src/**/*.jade'])
-     .pipe(changed('dist', { extension: '.html' }))
-     .pipe(debug({ title: '编译：' }))
-     .pipe(jade({ pretty: true }))
-     .pipe(gulp.dest('dist'))
-     .pipe(browserSync.reload({ stream: true }));
- });
-
-
- //清理
- gulp.task('clean', function() {
-   return gulp.src('dist', { read: false })
-     .pipe(clean());
- })
+//jade
+gulp.task('jade', function () {
+  return gulp.src(['src/*.jade', 'src/**/*.jade'])
+    .pipe(changed('dist', { extension: '.html' }))
+    .pipe(debug({ title: '编译：' }))
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.reload({ stream: true }));
+});
 
 
- gulp.task('build', function() {
-   gulp.start('css', 'js', 'img', 'jade', 'plugs');
- });
+//清理
+gulp.task('clean', function () {
+  return gulp.src('dist', { read: false })
+    .pipe(clean());
+})
 
- gulp.task('serve', ['css', 'js', 'img', 'jade', 'plugs'], function() {
-   browserSync.init({
-     server: {
-       baseDir: 'dist'
-     }
-   });
 
-   gulp.watch(['src/*.jade', 'src/**/*.jade'], ['jade'])
-   gulp.watch(['src/skin/scss/*.scss', 'src/skin/scss/**/*.scss'], ['css']);
-   gulp.watch(['src/skin/js/*.js', 'src/skin/js/**/*.js'], ['js']);
-   gulp.watch(['src/skin/img/*', 'src/skin/img/**/*'], ['img']);
-   gulp.watch('*').on('change', browserSync.reload);
- })
+gulp.task('build', function () {
+  gulp.start('css', 'js', 'img', 'jade', 'plugs');
+});
 
- gulp.task('default', ['serve'])
+gulp.task('serve', ['css', 'js', 'img', 'jade', 'plugs'], function () {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+
+  gulp.watch(['src/*.jade', 'src/**/*.jade'], ['jade'])
+  gulp.watch(['src/skin/scss/*.scss', 'src/skin/scss/**/*.scss', 'src/skin/promotion/**/*.scss'], ['css']);
+  gulp.watch(['src/skin/js/*.js', 'src/skin/js/**/*.js'], ['js']);
+  gulp.watch(['src/skin/img/*', 'src/skin/img/**/*'], ['img']);
+  gulp.watch('*').on('change', browserSync.reload);
+})
+
+gulp.task('default', ['serve'])
--- src/skin/js/mycenter/msgCenter.js
+++ src/skin/js/mycenter/msgCenter.js
@@ -1,90 +1,211 @@
 var msgModel = new Vue({
   el: '#msg-center',
   data: {
+    pageSize: 1, // 每页显示条数
+    curPage: 1, // 当前页
+    Pagedata: '',
     list: '',
   },
   computed: {
     /**
      * 返回评论列表
      */
-    comList:function  () {
-      var comN =[], // 
-          comS =[],
-          comL = this.list?this.list:[];
-          for(var i = 0; i<comL.length; i++){
-            // 1.1 父级评论
-            if(comL[i].ParentID == 0 ){
-              comL[i].comSon=[];
-              comN.push(comL[i]);
-            }else{
-            // 1.2 二级评论
-            comS.push(comL[i]);          
-            }
-          }
+    comList: function () {
+      var comN = [], // 
+        comS = [],
+        comL = this.list ? this.list : [];
+      for (var i = 0; i < comL.length; i++) {
+        // 1.1 父级评论
+        if (comL[i].ParentID == 0) {
+          comL[i].comSon = [];
+          comN.push(comL[i]);
+        } else {
+          // 1.2 二级评论
+          comS.push(comL[i]);
+        }
+      }
 
-          for (var j = 0; j < comN.length; j++) {
-            for(var i = 0; i<comS.length;i++){
-              if(comS[i].ParentID == comN[j].ID){
-                comN[j].comSon.push(comS[i]);
-              }
-            }
+      for (var j = 0; j < comN.length; j++) {
+        for (var i = 0; i < comS.length; i++) {
+          if (comS[i].ParentID == comN[j].ID) {
+            comN[j].comSon.push(comS[i]);
           }
+        }
+      }
 
       return comN;
+    },
+    // 分页数组
+    PagesIndex: function () {
+      return DGDTOOLS.Ev._setPage(this.totalPage, this.curPage);
+    },
+    // 总页数
+    totalPage: function () {
+      return this.Pagedata.pagecount
+      // return 15;
     }
   },
   methods: {
     // 获取消息列表
-    getMsgList: function () {
+    /**
+     * t1 : loding 方式
+     * t2 : pc || mobile 
+     * t3 : 回复追加数据
+     * id : 回复对应的id
+     */
+    getMsgList: function (t1, t2, t3, id) {
       var _this = this;
+      t1 ? headerModel.loading = true : $("#loader").fadeIn(300);
       $.ajax({
-        url: headerModel.api + '/MyCenter/GetMessageList',
+        url: headerModel.api + '/MyCenter/MyCenter_DelMessage',
         data: {
           "Token": headerModel.token,
           "MonthNum": 0,
-          "currentpage": 1,
-          "pagesize": 3
+          "currentpage": _this.curPage,
+          "pagesize": _this.pageSize
         },
         type: 'post',
         success: function (response) {
-          $("#loader").fadeOut(300)
+          t1 ? headerModel.loading = false : $("#loader").fadeOut(300)
           if (response.resultid === 200) {
-            _this.list = response.RelpyList;
-            console.log(_this.list)
+            _this.Pagedata = response;
+            if (t2) {
+              if (t3) {
+                // unshift
+                _this.list.pop({
+                  'Account': mySideModel.userBaseInfo.UserName,
+                  'Avatar': mySideModel.userBaseInfo.Avatar,
+                  'UserID': mySideModel.userBaseInfo.uid,
+                  'PostDate': '刚刚',
+                  'Content': _this.comTxt,
+                  'ParentID': id,
+
+                })
+              } else {
+                _this.list = _this.list.concat(response.RelpyList)
+
+              }
+
+            } else {
+              _this.list = response.RelpyList;
+            }
           } else {
             DGDTOOLS.tip._tip(response.message);
           }
         },
         error: function (e) {
-          $("#loader").fadeOut(300)
+          t1 ? headerModel.loading = false : $("#loader").fadeOut(300)
           DGDTOOLS.tip._tip(e.status + ":" + '接口异常')
           console.error(e.status + ":" + e.responseText);
         }
       })
     },
-    // 获取回复消息列表
-    getRepMsg: function  () {
+    // 显示二级评论框
+    toggleRepay: function (id, e) {
       var _this = this;
+      var appendDiv = $(e.target).parents('.com-cont'),
+        subForm = '<div class="com-form"><div class="com-inner"><textarea placeholder="来说两句吧……" value="" class="com-box"></textarea></div> <div class="com-ft clearfix"><!----> <button data-color="#fff">发表评论</button></div></div>';
+      if (appendDiv.find('.com-form').length) {
+        appendDiv.find('.com-form').toggle();
+        return;
+      } else {
+        appendDiv.append(subForm);
+      }
+      appendDiv.find('button').click(function () {
+
+        _this.comTxt = appendDiv.find('textarea').val();
+        _this.postComment(id, appendDiv.find('textarea'));
+      })
+    },
+    checkComment: function (obj) {
+      var _this = this;
+      if (!headerModel.isLogin) {
+        DGDTOOLS.check._isLogin();
+        return false;
+      }
+      if (obj.val() == '') {
+        DGDTOOLS.tip._tip('请说两句。。。', function () {
+          obj.focus();
+        })
+        return false;
+      }
+      return true;
+    },
+    // 提交评论信息
+    postComment: function (id, obj) {
+      var _this = this;
+      var pId = id ? id : 0;
+      obj = obj ? obj : $(_this.$refs.comTxt);
+      _this.comTxt = obj.val();
+      // 1.1 检测登录
+      if (!_this.checkComment(obj)) {
+        return
+      }
+      // newsid = 7431
+      headerModel.loading = true;
       $.ajax({
-        url: headerModel.api + '/MyCenter/GetMessageList',
+        url: headerModel.api + '/MyCenter/ReplyMessage',
         data: {
-          "Token": headerModel.token,
-          "MonthNum": 0,
-          "currentpage": 1,
-          "pagesize": 3
+          "token": headerModel.token,
+          "Content": _this.comTxt,
+          "MessageID": pId,
         },
         type: 'post',
         success: function (response) {
-          $("#loader").fadeOut(300)
+          headerModel.loading = false;
+          if (response.resultid == 200) {
+            // 提交评论成功 拼接到二级评论前面。
+            obj.val('');
+            if (headerModel.isMobileOrTable) {
+              _this.getMsgList(1, 'mb', 'oppend', pId)
+            } else {
+              _this.getMsgList();
+
+            }
+
+          } else {
+            DGDTOOLS.tip._tip(response.message)
+          }
+        },
+        error: function (e) {
+          headerModel.loading = false;
+          DGDTOOLS.tip._tip(e.status + ":" + '接口异常')
+          console.error(e.status + ":" + e.responseText);
+        }
+      })
+    },
+    /* 翻页事件 */
+    pageClick: function (index, type) {
+      if (index != this.curPage) {
+        this.curPage = index;
+        type ? this.getMsgList(1, 'mb') : this.getMsgList(1);
+      }
+    },
+    // 删除指定的站内信
+    delMsg: function (id, e) {
+      var _this = this;
+      console.log(id)
+      headerModel.loading = true;
+      $.ajax({
+        url: headerModel.api + '/MyCenter/DelMessage',
+        data: {
+          "Token": headerModel.token,
+          "MsgID": id
+        },
+        type: 'post',
+        success: function (response) {
+          headerModel.loading = false
           if (response.resultid === 200) {
-            _this.list = response.response;
-            console.log(_this.list)
+            DGDTOOLS.tip._tipAll({
+              type: 'ok',
+            })
+
           } else {
             DGDTOOLS.tip._tip(response.message);
           }
         },
         error: function (e) {
-          $("#loader").fadeOut(300)
+          headerModel.loading = false
           DGDTOOLS.tip._tip(e.status + ":" + '接口异常')
           console.error(e.status + ":" + e.responseText);
         }
